d1fb7c9247d5b178c8cfeee18015471ae253602d
VrsajkovIvan33 <vrsajkov.ivan@gmail.com>
2016-09-08 17:22:32 +0200

    Update workdir-test.sh

--- test/thrift-version-test.sh
+++ test/thrift-version-test.sh
@@ -5,7 +5,7 @@ OUTPUT=$(docker exec -ti etd_container sh -c "thrift -version")
 
 if [[ "${OUTPUT}" =~ "Thrift version 0.9.3" ]]
 then
-	echo "Correct version (0.9.3) installed"
+	echo "Correct version (0.9.3) installed!"
 	exit 0
 else
 	echo "Correct version (0.9.3) is not installed!"
--- test/workdir-test.sh
+++ test/workdir-test.sh
@@ -2,11 +2,10 @@
 
 echo "Checking if the parent directory is called 'release'"
 WORKDIR=$(docker exec -ti etd_container sh -c "pwd")
-echo "workdir: ${WORKDIR}"
-if [[ ${WORKDIR} == "/release" ]]; then
-  echo "Success!"
+if [[ ${WORKDIR} =~ "/release" ]]; then
+  echo "Correct name of the parent directory!"
   exit 0
 else
-  echo "Failure!"
+  echo "Wrong name of the parent directory!"
   exit 1
 fi
