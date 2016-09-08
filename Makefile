38ab7e86b6dd400c273266a110e18d7e9a7df04f
VrsajkovIvan33 <vrsajkov.ivan@gmail.com>
2016-09-08 10:25:48 +0200

    Use bash instead of sh

--- Makefile
+++ Makefile
@@ -2,6 +2,6 @@ image:
 	docker build -t elixir-thrift-dev .
 	docker run --name et_container -tdi elixir-thrift-dev
 test:
-	sh test/test.sh
+	bash test/test.sh
 release:
-	sh push-to-dockerhub.sh
+	bash push-to-dockerhub.sh
--- test/test.sh
+++ test/test.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 for file in $(ls); do
-    if [ "$file" != "test.sh" ]; then
+    if [[ "$file" != "test.sh" ]]; then
         echo "$file"
         bash "$file"
         RETVAL=$?
-        if [ ${RETVAL} != 0 ]; then
+        if [[ ${RETVAL} != 0 ]]; then
           echo "Test $file failed! Exiting..."
           exit 1
         fi
