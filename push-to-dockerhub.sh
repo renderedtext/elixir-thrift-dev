884a167df38c77b6a5922787cab0659c4d4d47d7
VrsajkovIvan33 <vrsajkov.ivan@gmail.com>
2016-09-08 10:12:18 +0200

    Login to Docker Hub before pushing the image

--- push-to-dockerhub.sh
+++ push-to-dockerhub.sh
@@ -8,10 +8,12 @@ docker pull "${img_name}${img_version}"
 
 if [[ $? == 1 ]]; then
     echo "Image of the specified version doesn't exist in the repository. Pushing the specified version and updating the latest image."
+    docker login --username $DH_USERNAME --password $DH_PASSWORD
     docker tag renderedtext/elixir-thrift-dev ${img_name}${img_version}
     docker push ${img_name}${img_version}
     docker tag ${img_name}${img_version}  ${img_name}latest
     docker push ${img_name}latest
+    docker logout
 else
     echo "Image of the specified version already exists in the repository."
 fi
